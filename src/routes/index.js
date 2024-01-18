2e49ffa2fdfdfa57caf552f266bda55c64d9a6c6
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-01-18 13:48:48 +0530

    Add reminder send functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,12 @@ router.get('/', (req, res) => res.send('Hello World!'));
 router.get('/reminder-refresh', reminderController.refreshReminders);
 
 router.get('/reminders', reminderController.getReminders);
+
 router.post('/reminders/add', reminderController.addReminder);
 router.post('/reminders/:id', reminderController.updateReminder);
 router.delete('/reminders/:id', reminderController.deleteReminder);
+
+// upstash
 router.post("/reminders-send", reminderController.sendReminders);
 
 /**
